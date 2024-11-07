24975a42eb6ae20451bbe1f5077f9c18ce883d22
Mohammad Baker Tarhini <149156843+Mohammad-Tarhini@users.noreply.github.com>
2024-11-07 09:06:33 +0200

    Update overlapping_intervals1.py

--- overlapping_intervals1.py
+++ overlapping_intervals1.py
@@ -1,2 +1,16 @@
 def merge_intervals(intervals):
+   i=0
+   while i<len(intervals):
+      j+=1
+      while j<len(intervals):
+         if intervals[i][0]<=intervals[j][0] and intervals[j][1]>=intervals[j][1]:
+            intervals.pop(j)
+         elif intervals[i][1]>intervals[j][0] and intervals[i][0]<intervals[j][1]:
+            intervals[i][0]=min(intervals[i][0],intervals[j][0])
+            intervals[i][1]=max(intervals[i][1],intervals[j][1])
+            intervals.pop[j]
+         else:
+            j+=1
+      i+=1
+      
     
